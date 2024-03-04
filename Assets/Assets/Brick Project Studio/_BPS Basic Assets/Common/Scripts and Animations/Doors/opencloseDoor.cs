5dab18b8c1605b9e8d2f97321e26806a29709672
guarm22 <60443541+guarm22@users.noreply.github.com>
2024-03-03 21:24:36 -0500

    assets

---
+++ Assets/Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+
+{
+	public class opencloseDoor : MonoBehaviour
+	{
+
+		public Animator openandclose;
+		public bool open;
+		public Transform Player;
+		public int minDistance = 15;
+
+		void Start()
+		{
+			open = false;
+		}
+
+		void OnMouseOver()
+		{
+			{
+				if (Player)
+				{
+					float dist = Vector3.Distance(Player.position, transform.position);
+					if (dist < minDistance)
+					{
+						if (open == false)
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								StartCoroutine(opening());
+							}
+						}
+						else
+						{
+							if (open == true)
+							{
+								if (Input.GetMouseButtonDown(0))
+								{
+									StartCoroutine(closing());
+								}
+							}
+
+						}
+
+					}
+				}
+
+			}
+
+		}
+
+		IEnumerator opening()
+		{
+			openandclose.Play("Opening");
+			open = true;
+			yield return new WaitForSeconds(.5f);
+		}
+
+		IEnumerator closing()
+		{
+			openandclose.Play("Closing");
+			open = false;
+			yield return new WaitForSeconds(.5f);
+		}
+
+
+	}
+}
